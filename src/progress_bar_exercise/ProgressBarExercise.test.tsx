ff12814556c1a89116d6137bf0a43fd0a927a922
annaliarosed <annaliadestefano@gmail.com>
2021-07-26 10:45:16 +0300

    write tests

--- src/progress_bar_exercise/ProgressBarExercise.test.tsx
+++ src/progress_bar_exercise/ProgressBarExercise.test.tsx
@@ -1,9 +1,42 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import React from "react";
 import ProgressBarExercise from "./ProgressBarExercise";
 
 describe("<Solution />", () => {
-  test("renders", () => {
+  test("renders", async () => {
     render(<ProgressBarExercise />);
+
+    const startButton = screen.getByRole("button", {
+      name: /start request/i,
+    });
+
+    userEvent.click(startButton);
+
+    const progressBar = await screen.findByTestId("progressBar");
+
+    expect(progressBar).toBeVisible();
+  });
+
+  test("should finish and disappear on finish click", async () => {
+    render(<ProgressBarExercise />);
+
+    const startButton = screen.getByRole("button", {
+      name: /start request/i,
+    });
+
+    userEvent.click(startButton);
+
+    const progressBar = await screen.findByTestId("progressBar");
+
+    expect(progressBar).toBeVisible();
+
+    const finishRequestButton = screen.getByRole("button", {
+      name: /finish request/i,
+    });
+
+    userEvent.click(finishRequestButton);
+
+    expect(progressBar).not.toBeVisible();
   });
 });
